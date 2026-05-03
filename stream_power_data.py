bc1db1d36e601edddfb8e011956f7882fee21f07
whiterabbit077 <jupyter-agiczew@ncsu.edu@jhub.cos.ncsu.edu>
2026-05-03 06:33:45 -0400

    added Spark ML transformation and feature piepline

--- stream_power_data.py
+++ stream_power_data.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 # read the streaming data file
@@ -9,3 +10,11 @@ print(stream_pd.shape)
 
 print("\nfirst few rows:")
 print(stream_pd.head())
+
+output_folder = "stream_data"
+os.makedirs(output_folder, exist_ok=True)
+
+sample_df = stream_pd.sample(n=5, replace=False)
+sample_df.to_csv(f"{output_folder}/test_batch.csv", index=False)
+
+print("wrote one test csv file")
