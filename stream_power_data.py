79c2211712872427967e841d1afb4f1cdb4de177
whiterabbit077 <jupyter-agiczew@ncsu.edu@jhub.cos.ncsu.edu>
2026-05-03 07:45:57 -0400

    read csv stream and print model predictions to console, added gitignore

--- stream_power_data.py
+++ stream_power_data.py
@@ -8,9 +8,9 @@ stream_pd = pd.read_csv("power_streaming_data.csv")
 output_folder = "stream_data"
 os.makedirs(output_folder, exist_ok=True)
 
-for i in range(3):
+for i in range(20):
     sample_df = stream_pd.sample(n=5, replace=False)
-    file_name = f"{output_folder}/batch_{i}.csv"
+    file_name = f"{output_folder}/batch_{i+1}.csv"
     sample_df.to_csv(file_name, index=False)
     print("wrote", file_name)
-    time.sleep(2)
+    time.sleep(10)
