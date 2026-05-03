9bff4d2bf50a95f52ad47b7f3feb4ea0d2ea1eb3
whiterabbit077 <jupyter-agiczew@ncsu.edu@jhub.cos.ncsu.edu>
2026-05-03 07:06:27 -0400

    fit elastic net with cross validation and evaluate model

--- stream_power_data.py
+++ stream_power_data.py
@@ -1,20 +1,16 @@
 import os
+import time
 import pandas as pd
 
 # read the streaming data file
 stream_pd = pd.read_csv("power_streaming_data.csv")
 
-# check that it loaded correctly
-print("streaming data shape:")
-print(stream_pd.shape)
-
-print("\nfirst few rows:")
-print(stream_pd.head())
-
 output_folder = "stream_data"
 os.makedirs(output_folder, exist_ok=True)
 
-sample_df = stream_pd.sample(n=5, replace=False)
-sample_df.to_csv(f"{output_folder}/test_batch.csv", index=False)
-
-print("wrote one test csv file")
+for i in range(3):
+    sample_df = stream_pd.sample(n=5, replace=False)
+    file_name = f"{output_folder}/batch_{i}.csv"
+    sample_df.to_csv(file_name, index=False)
+    print("wrote", file_name)
+    time.sleep(2)
